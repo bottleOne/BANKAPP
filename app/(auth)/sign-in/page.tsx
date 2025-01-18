29918d292247ab5067e8570fb80f4e332647c51d
전병일 <jeonbye123@gmail.com>
2025-01-18 20:23:37 +0900

    기초세팅

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SinIn = () => {
+    return (
+        <div>
+            SinIn
+        </div>
+    );
+};
+
+export default SinIn;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SinUp = () => {
+    return (
+        <div>
+            SinUp
+        </div>
+    );
+};
+
+export default SinUp;
